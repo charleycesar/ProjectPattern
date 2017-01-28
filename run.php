1ca9edf7fa90942190dacaeba3fac69d0f607cb2
Charley <charleycesar@gmail.com>
2017-01-28 14:33:09 -0200

    Criando Classe Html

--- run.php
+++ run.php
@@ -1,3 +1,7 @@
 <?php
     include "vendor/autoload.php";
+
     $html = new Projeto\Html\Html;
+
+    $img = $html->img('logo.png',['class'=>'btn btn-primary']);
+    echo $html->a($img, 'https://charleycesar.github.io', ['class'=>'btn btn-primary']);
--- src/Html.php
+++ src/Html.php
@@ -1,5 +1,30 @@
 <?php
-    namespace Project\Html;
-    class Html {
     
+    namespace Projeto\Html;
+    
+    class Html {
+        
+        public function a($text,$href, array $attribute = null){
+	    $attribute = $this->$attribute($attribute);
+	    return "<a href='{$href}' {$attribute}>{$text}</a>";        
+        }
+
+	public function img($src, array $attribute = null){
+	    return "<img src='{$src}' {$attribute}>";
+	}
+
+	public function attribute(array $attribute = null){
+	    if( $attribute !== null ){
+		foreach( $attribute as $k => $v ){
+		    if( is_int($k) ){
+		    	$data[] => $v;
+		    }else{ 
+		    	$data[] => $k. '="' .$v. '"';
+		    }
+		}
+		$attribute = implode(' ',$data);
+	    } 
+	    return $attribute;
+	}	
+        
     }
